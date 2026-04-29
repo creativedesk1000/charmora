34e2b3f9cf99468d4fdb7a5cc9372c94071cb3f5
Ahtesham Asif <creativedesk1000@gmail.com>
2026-04-29 23:05:23 +0500

    solve db issue

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "opfaxotdajkhrhhzqvxk.supabase.co",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
